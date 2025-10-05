6ec0b659a194638ee52280719bbde3c106802488
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-05 12:44:40 +0530

    Create Operations.py

---
+++ Operations.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum_result = a + b
+diff_result = a - b
+print("Sum: ", sum_result)
+print("Subtraction: ", diff_result)
