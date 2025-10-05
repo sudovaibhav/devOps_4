ee5f1542156b96189bfbc4e4206991670dea7a6f
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2025-10-05 12:55:28 +0530

    Initial repository setup

---
+++ mult_div_operation.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+mult_result = a * b
+div_result = a / b
+print("Multiplication: ", mult_result)
+print("Division: ", div_result)
